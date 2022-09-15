1d6a169c5c33f42f4fc50f10f43180616387258c
thatmattlove <matt@stellar.tech>
2022-09-15 16:52:19 -0400

    change package name to confirm with eslint's dumb standards

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,53 @@
-import { vendor } from "./vendor";
-import { custom } from "./custom";
-
 import type { Linter } from "eslint";
 
-export default { ...vendor, ...custom } as Linter.Config;
+export const custom: Linter.Config = {
+    settings: {
+        files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    },
+    extends: ["airbnb", "prettier"],
+    parserOptions: { project: "./tsconfig.json" },
+    ignorePatterns: ["node_modules", "dist", "jest.config.ts", "*.spec.ts", "*.test.ts"],
+    plugins: ["@typescript-eslint"],
+    root: true,
+    rules: {
+        "@typescript-eslint/quotes": [
+            "error",
+            "double",
+            { avoidEscape: true, allowTemplateLiterals: true },
+        ],
+        "@typescript-eslint/return-await": "off",
+        "no-restricted-syntax": "off",
+        "no-return-await": "off",
+        "@typescript/return-await": "off",
+        "consistent-return": "off",
+        "no-restricted-globals": "off",
+        "@typescript-eslint/indent": "off",
+        "import/prefer-default-export": "off",
+        "no-underscore-dangle": "off",
+        "default-case": "off",
+        "object-curly-newline": "off",
+        "no-console": "off",
+        "max-classes-per-file": "off",
+        "class-methods-use-this": "off",
+        "import/no-cycle": "off",
+        "no-nested-ternary": "off",
+        "no-promise-executor-return": "off",
+        "no-await-in-loop": "off",
+        "no-param-reassign": ["error", { props: false }],
+        "no-continue": "off",
+        "import/order": [
+            "error",
+            {
+                groups: [
+                    "builtin",
+                    "external",
+                    "internal",
+                    "index",
+                    ["sibling", "parent"],
+                    "unknown",
+                    "type",
+                ],
+            },
+        ],
+    },
+};
