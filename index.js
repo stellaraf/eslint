ef2edd34c186bad111483476c7412182f0fa48b3
thatmattlove <matt@stellar.tech>
2023-05-01 13:57:12 -0400

    update deps, add rules

--- index.js
+++ index.js
@@ -1,107 +1,105 @@
-/** @type {import("eslint/conf/eslint-all")} */
+/** @type {import("eslint/lib/shared/types")} */
 module.exports = {
-    settings: {
-        files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
-        "import/resolver": {
-            node: {
-                extensions: [".js", ".jsx", ".ts", ".tsx"],
-            },
-        },
-        "import/parsers": {
-            "@typescript-eslint/parser": [".ts", ".tsx"],
-        },
-        "import/resolver": {
-            typescript: {
-                alwaysTryTypes: true,
-                project: ["tsconfig.json", "package/tsconfig.json"],
-            },
-            node: {
-                project: ["tsconfig.json", "package/tsconfig.json"],
-            },
-        },
+  settings: {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: ["tsconfig.json", "package/tsconfig.json"],
+      },
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
     },
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+  },
 
-    parser: "@typescript-eslint/parser",
-    extends: [
-        "airbnb-base",
-        "prettier",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-    ],
-    parserOptions: { project: "./tsconfig.json" },
-    ignorePatterns: [
-        "node_modules",
-        "dist",
-        "jest.config.ts",
-        "*.spec.ts",
-        "*.test.ts",
-        "build.[jt]s",
-    ],
-    plugins: ["import", "@typescript-eslint"],
-    root: true,
-    overrides: [
-        {
-            files: ["*.ts", "*.tsx"],
-            rules: {
-                "no-undef": "off",
-            },
-        },
-    ],
-    rules: {
-        "no-shadow": "off",
-        "@typescript-eslint/no-shadow": "error",
-        "@typescript-eslint/quotes": [
-            "error",
-            "double",
-            { avoidEscape: true, allowTemplateLiterals: true },
-        ],
-        "import/no-extraneous-dependencies": [
-            "error",
-            { devDependencies: ["**/*.test.[jt]s", "**/*.spec.[jt]s", "build.[jt]s"] },
-        ],
-        "@typescript-eslint/return-await": "off",
-        "no-restricted-syntax": "off",
-        "no-return-await": "off",
-        "@typescript/return-await": "off",
-        "consistent-return": "off",
-        "no-restricted-globals": "off",
-        "@typescript-eslint/indent": "off",
-        "import/prefer-default-export": "off",
-        "no-underscore-dangle": "off",
-        "default-case": "off",
-        "object-curly-newline": "off",
-        "no-console": "off",
-        "max-classes-per-file": "off",
-        "class-methods-use-this": "off",
-        "import/no-cycle": "off",
-        "no-nested-ternary": "off",
-        "no-promise-executor-return": "off",
-        "no-await-in-loop": "off",
-        "no-param-reassign": ["error", { props: false }],
-        "no-continue": "off",
-        "import/extensions": [
-            "error",
-            "ignorePackages",
-            {
-                js: "never",
-                jsx: "never",
-                ts: "never",
-                tsx: "never",
-            },
-        ],
-        "import/order": [
-            "error",
-            {
-                groups: [
-                    "builtin",
-                    "external",
-                    "internal",
-                    "index",
-                    ["sibling", "parent"],
-                    "unknown",
-                    "type",
-                ],
-            },
-        ],
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "airbnb-base",
+    "prettier",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parserOptions: { project: "./tsconfig.json" },
+  ignorePatterns: [
+    "node_modules",
+    "dist",
+    "jest.config.ts",
+    "*.spec.ts",
+    "*.test.ts",
+    "build.[jt]s",
+  ],
+  plugins: ["import", "@typescript-eslint"],
+  root: true,
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "no-undef": "off",
+      },
     },
+  ],
+  rules: {
+    "no-shadow": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/indent": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/no-shadow": "error",
+    "@typescript-eslint/no-unnecessary-type-constraint": "off",
+    "@typescript-eslint/quotes": [
+      "error",
+      "double",
+      { avoidEscape: true, allowTemplateLiterals: true },
+    ],
+    "@typescript-eslint/return-await": "off",
+    "@typescript/return-await": "off",
+    "class-methods-use-this": "off",
+    "consistent-return": "off",
+    "default-case": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "import/no-cycle": "off",
+    "import/no-extraneous-dependencies": [
+      "error",
+      { devDependencies: ["**/*.test.[jt]s", "**/*.spec.[jt]s", "build.[jt]s"] },
+    ],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "index",
+          ["sibling", "parent"],
+          "unknown",
+          "type",
+        ],
+      },
+    ],
+    "import/prefer-default-export": "off",
+    "max-classes-per-file": "off",
+    "no-await-in-loop": "off",
+    "no-console": "off",
+    "no-continue": "off",
+    "no-nested-ternary": "off",
+    "no-param-reassign": ["error", { props: false }],
+    "no-promise-executor-return": "off",
+    "no-restricted-globals": "off",
+    "no-restricted-syntax": "off",
+    "no-return-await": "off",
+    "no-underscore-dangle": "off",
+    "object-curly-newline": "off",
+  },
 };
